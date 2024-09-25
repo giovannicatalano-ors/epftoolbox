010db19a664c806dffe0a2254a1d7634ea73bcbc
giovannicatalano-ors <giovanni.catalano@dedagroup.it>
2024-09-25 17:17:39 +0200

    Modifiche

--- epftoolbox/models/__init__.py
+++ epftoolbox/models/__init__.py
@@ -1,4 +1,3 @@
 from ._lear import (LEAR, evaluate_lear_in_test_dataset)
 from ._dnn import (DNNModel, DNN, evaluate_dnn_in_test_dataset, format_best_trial)
-from ._dnn_hyperopt import (hyperparameter_optimizer)
-from ._dnn_hyperopt import(hyperparameter_optimizer_customized)
+from ._dnn_hyperopt import (hyperparameter_optimizer, hyperparameter_optimizer_customized)
